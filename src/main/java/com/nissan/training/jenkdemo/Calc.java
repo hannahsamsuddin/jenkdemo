5db507e43ccba3c603470c1f69e5899775b81ae7
hannahsamsuddin <hannah.s2911@gmail.com>
2018-08-17 11:27:52 +0530

    initial commit

---
+++ src/main/java/com/nissan/training/jenkdemo/Calc.java
@@ -0,0 +1,15 @@
+package com.nissan.training.jenkdemo;
+
+public class Calc {
+	
+	public int add(int x,int y)
+	{
+		return x+y;
+	}
+	
+	public int sub(int x,int y)
+	{
+		return x-y;
+	}
+
+}
---
+++ src/test/java/com/nissan/training/jenkdemo/TestCalc.java
@@ -0,0 +1,20 @@
+package com.nissan.training.jenkdemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalc {
+
+	@Test
+	public void testadd() {
+		Calc c = new Calc();
+		assertEquals(5,c.add(2, 3));
+	}
+	
+	@Test
+	public void testsub() {
+		Calc c = new Calc();
+		assertEquals(1,c.sub(3, 2));
+	}
+}
